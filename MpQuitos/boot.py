93a3879d448e28fb9efd202c88690babeef861fb
j3den <me@jcovell.co.uk>
2018-12-27 08:26:06 +0000

    Adding AP setup

--- MpQuitos/boot.py
+++ MpQuitos/boot.py
@@ -1,5 +1,7 @@
 import gc
 import config.ConfigurationManager as cm
+import network
+import time as time
 
 print("Now Booting!")
 print("Instantiating ConfigurationManager...")
@@ -11,5 +13,21 @@ for e in configManager.getConfig():
 print("Enabling and Setting GC threshold.\n")
 gc.enable()
 gc.collect()
-gc.set_threshold((gc.mem_alloc() + gc.mem_free())/5)
+# gc.set_threshold((gc.mem_alloc() + gc.mem_free())/5)
 
+# Enable AP Wifi and scan:
+
+wifiInterface = network.WLAN(network.AP_IF)
+wifiInterface.active(True)
+wifiInterface.config(essid="MOSQUITOX")
+wifiInterface.config(authmode=3, password='1234567819')
+time.sleep(2)
+
+while wifiInterface.active is False:
+    print("Active = "+str(wifiInterface.active()))
+    time.sleep(1)
+while True:
+    networks = wifiInterface.scan()
+    for nw in networks:
+        print("Found " + nw.ssid)
+    time.sleep(3)
